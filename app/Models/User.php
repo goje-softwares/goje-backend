1de43092618a2d78f11bd7d062acdc3e325043e0
ashkanfekridev <ashkanfekridev@gmail.com>
2022-07-11 05:28:28 +0430

    update user model for add products method

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static where(string $string, string $string1)
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +44,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
