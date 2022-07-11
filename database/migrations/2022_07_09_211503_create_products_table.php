36d4e26377de7d424022fda347330dec2cf41266
ashkanfekridev <ashkanfekridev@gmail.com>
2022-07-11 05:26:06 +0430

    create product migration

---
+++ database/migrations/2022_07_09_211503_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->unsignedInteger('amount')->nullable();
+            $table->unsignedInteger('price')->default(0);
+            $table->unsignedInteger('status')->default(0);
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
