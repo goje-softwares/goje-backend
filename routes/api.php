2fe2d35d1ad0bcb4b02cdcedf320870fea0b7651
ashkanfekridev <ashkanfekridev@gmail.com>
2022-07-11 05:33:32 +0430

    update for add products create and store in api route

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,16 @@ Route::group([
         Route::post('login', [AuthController::class, 'login'])->name('login');
         Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout'])->name('logout');
     });
-
+});
+// Products
+Route::group([
+    'middleware' => ['auth:sanctum'],
+    'prefix' => 'products',
+    'as' => 'api.products.'
+], function () {
+//    http://localhost:8000/api/products
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::post('/', [ProductController::class, 'store'])->name('products.store');
 });
 
 
