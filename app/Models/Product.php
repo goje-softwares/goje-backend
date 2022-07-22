46b033320ca492febcacff0c25c3f397f74865f2
ashkan fekri <ashknfekridev@gmail.com>
2022-07-22 17:14:51 +0430

    update products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method create(\App\Http\Requests\API\StoreProductRequest $product)
+ */
 class Product extends Model
 {
     use HasFactory;
